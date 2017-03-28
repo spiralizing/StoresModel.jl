a83e35bd1c7681ed58d4c3aa3e2aa0192c35ffcb
Alfredo <spiralizing@gmail.com>
2017-03-28 10:52:00 -0600

    creating new files

---
+++ src/func_mod.jl
@@ -0,0 +1,12 @@
+#In this file are the functions for the simulations
+#
+function prob_site()
+
+end
+
+################################################################################
+#
+function market(pos::Array{Int64,1}, util::Float64, pb::)
+
+
+end
---
+++ src/sim_test.jl
@@ -0,0 +1,6 @@
+##################Simulation####################################################
+#loading the libraries
+using StoresModel
+#parameters
+dim = [10,10] #Lx, Ly
+bs = [buyer()]
